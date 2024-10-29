24131fd0947d3ac6a2f8606da04a1657b6d75b25
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-10-29 20:01:04 +0200

    Implemented basic API functionality

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,38 @@
 const express = require("express");
+const { randomUUID } = require("crypto");
+const { notes } = require("./data/notes");
+
 const app = express();
 const port = 3000;
 
-const { notes } = require("./data/notes");
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send(notes);
+app.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/notes/:noteId", (req, res) => {
+  const noteId = req.params.noteId;
+  const note = notes.find((n) => n.id === noteId);
+
+  if (!note) {
+    res.status(404).json({
+      ok: false,
+      message: "No post with the given ID.",
+    });
+  }
+
+  res.json(note);
+});
+
+app.post("/notes", (req, res) => {
+  const note = req.body;
+  const id = randomUUID();
+
+  const noteToCreate = { ...note, id };
+
+  notes.push(noteToCreate);
+  res.status(201).json(noteToCreate);
 });
 
 app.listen(port, () => {
